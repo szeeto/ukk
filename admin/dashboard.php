8bb327514114c0f81eb5d7fa88815f8218c0d5f1
unknown <patrasawali93@gmail.com>
2026-01-19 15:33:13 +0700

    .

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if(empty($_SESSION['id_admin'])){
+    header("location:../login-admin.php");
+    
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,6 +17,23 @@
     <div class="container mt-3 mb-3">
         <h4>halaman Admin | Aplikasi Perpustakaan Sekolah Digital</h4>
         <a href="dashboard.php" class="btn btn-success text-white">Dashboarad</a>
+        <a href="?halaman=data_buku" class="btn btn-success text-white">Buku</a>
+        <a href="?halaman=data_anggota" class="btn btn-success text-white">Anggota</a>
+        <a href="?halaman=data_peminjaman" class="btn btn-success text-white">Peminjaman</a>
+        <a href="logout.php" class="btn btn-danger text-white">Logout</a>
+        <div class="card mt-3 p-3">
+            <?php
+            $halaman = isset($_GET['halaman']) ? $_GET['halaman']:"";
+            if(file_exists($halaman.".php")){
+                include $halaman.".php";
+            }else{
+                ?>
+                <h4>Selamat Datang <?= $_SESSION['nama_lengkap'];?>👋</h4>
+                <p class="text-jusify text-muted">
+                    Aplikasi Perpustakaan Sekolah Digital Merupakan Sistem Berbasis Web Yang Dirancang Untuk Membantu Pengelolaan Data Buku, Data Anggota Dan Peminjaman Terorganisir
+                </p>
+            <?php }?>
+        </div>
     </div>
 </body>
 </html>
